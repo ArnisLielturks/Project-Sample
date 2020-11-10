24f1058788ba7ba80c980d3d0a6dfbda790589d8
Arnis Lielturks <arnis.lielturks@gmail.com>
2020-11-10 22:26:16 +0200

    extended sample

---
+++ JS/LoadTextures.js
@@ -0,0 +1,7 @@
+Module.LoadResourceFromUrl("https://www.luckeyproductions.nl/23.png", "Textures/Logo.png");
+
+// Decodes base64 to binary and writes it in the engine
+var content = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAgMAAAAhHED1AAAADFBMVEUAAAAA/wD/gAD///9GPgw8AAAAt0lEQVR42u3auw3DMAwFQC6pJbVkUhkwCH3iuDJ9ryShKwmJUHxuJgAAAAAAALwOaK01AADwLiBS8uFRHwAA1AX6KRERBzDqAQCAmsAxJHrKrH5GAABAPSA3RwcAAEBtIA+N86NiVct1AABQA+h/BgAA1AV24HTQAACAMsAvyOUbCgAAeByQFxCxyXaDAQAAygAr5NJGEwAAPBIYIbtsf0AAAIBSwOrxAQAA6gLLT82TOgAAKAV8AZOgiB0hDSCxAAAAAElFTkSuQmCC"
+Module.AddResourceFromBase64("Textures/StoneDiffuse.dds", content);
+
+Module.LoadResourceFromUrl("https://urho3d.github.io/assets/images/logo.png","Textures/Mushroom.dds");
